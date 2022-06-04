b095ca8f652755b0819a203a7ad100c606bf09b7
hamamullaiqi <hamamullaiqi@gmail.com>
2022-06-04 20:29:06 +0700

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import "./assets/scss/style.scss"
+import "./assets/scss/style.scss";
+import {  Route, Routes } from "react-router-dom";
+import LandingPage from "pages/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route exact path="/" element={<LandingPage path="/" />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './parts/Header'
+
+export default function LandingPage(props) {
+
+  return (
+    <>
+        <Header {...props} />
+    </>
+  )
+}
